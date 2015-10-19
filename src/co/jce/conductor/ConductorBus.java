245df1fe4b4df96a175c78c3de99cfb9dfc9af43
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-18 19:38:11 -0500

    Crea las clases "ConductorBus" y "ConductorTaxi"
     - Crea las clases con las siguientes características:
       + Las dos clases heredan de la clase "Conductor"
       + Cada clase posee su propio constructor, en el cual se hace
         referencia al constructor de la clase padre.

---
+++ src/co/jce/conductor/ConductorBus.java
@@ -0,0 +1,10 @@
+package co.jce.conductor;
+
+public class ConductorBus extends Conductor {
+
+	public ConductorBus(int id, String nombre, String tipoLicencia) {
+		super(id, nombre, tipoLicencia);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/co/jce/conductor/ConductorTaxi.java
@@ -0,0 +1,10 @@
+package co.jce.conductor;
+
+public class ConductorTaxi extends Conductor {
+
+	public ConductorTaxi(int id, String nombre, String tipoLicencia) {
+		super(id, nombre, tipoLicencia);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
