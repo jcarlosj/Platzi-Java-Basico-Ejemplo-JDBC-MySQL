58e636847feaecbe843bdd3495e9f4134cbf566b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-18 19:25:34 -0500

    Crea las clases "Bus" y "Taxi"
     - Crea las clases con las siguientes características:
       + Las dos clases heredan de la clase "Vehículo"
       + Cada clase posee su propio constructor, en el cual se hace
         referencia al constructor de la clase padre.

---
+++ src/co/jce/vehiculo/Bus.java
@@ -0,0 +1,10 @@
+package co.jce.vehiculo;
+
+public class Bus extends Vehiculo {
+
+	public Bus(String matricula, String marca, String modelo) {
+		super(matricula, marca, modelo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/co/jce/vehiculo/Taxi.java
@@ -0,0 +1,10 @@
+package co.jce.vehiculo;
+
+public class Taxi extends Vehiculo {
+
+	public Taxi(String matricula, String marca, String modelo) {
+		super(matricula, marca, modelo);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
